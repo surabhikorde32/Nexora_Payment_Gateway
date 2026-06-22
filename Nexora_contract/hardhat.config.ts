e01981a65273426f98a05c774c58dbd31875889a
astha013 <asthapawar0138@gmail.com>
2026-06-22 17:53:09 +0530

    implement safe wallet key generation

--- Nexora_contract/hardhat.config.ts
+++ Nexora_contract/hardhat.config.ts
@@ -1,8 +1,8 @@
 import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
+import "@nomicfoundation/hardhat-ethers";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.28",
+  solidity: "0.8.28"
 };
 
 export default config;
